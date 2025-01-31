6526612b1f7c9049dca59987d7e6592c393618d9
keziei <40326921+keziei@users.noreply.github.com>
2025-01-31 17:21:30 +0000

    Terraform Lab

--- aws/vpc/main.tf
+++ aws/vpc/main.tf
@@ -95,3 +95,26 @@ resource "aws_nat_gateway" "nat" {
     Name = "NAT Gateway"
   }
 }
+
+resource "aws_route_table" "private_route" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0" # Default route for internet access
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
+  tags = {
+    Name = "Private Route"
+  }
+}
+
+resource "aws_route_table_association" "private_app" {
+  subnet_id      = aws_subnet.private_app.id
+  route_table_id = aws_route_table.private_route.id
+}
+
+resource "aws_route_table_association" "private_db" {
+  subnet_id      = aws_subnet.private_db.id
+  route_table_id = aws_route_table.private_route.id
+}
